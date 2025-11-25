5e20167c518fdb066516ec028db335c021f63947
moute40 <famajor77@gmail.com>
2025-11-25 11:55:14 -0500

    completation de classe ariere plan

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.tp2_s3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jdk.compiler;
 
 
     opens org.example.tp2_s3 to javafx.fxml;
--- src/main/java/org/example/tp2_s3/ArrierePlan.java
+++ src/main/java/org/example/tp2_s3/ArrierePlan.java
@@ -1,13 +1,34 @@
 package org.example.tp2_s3;
 
+import com.sun.tools.javac.Main;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 public class ArrierePlan implements Drawable {
+    private double tailleImageX = 192, tailleImageY = 96;
+    private Image imageBrique = new Image(getClass().getResourceAsStream("/images/brique.png"));
 
     public ArrierePlan() {
-        //todo
+
+    }
+    @Override
+    public void draw(GraphicsContext context) {
+        context.setFill(Color.BLACK);
+        context.fillRect(0, 0, MainJavaFx.WIDTH, MainJavaFx.HEIGHT);
+
+        double nombreImageBriqueEnX = MainJavaFx.WIDTH / tailleImageX;
+        double nombreImageBriqueEnY = MainJavaFx.HEIGHT / tailleImageY;
+
+        int nbX = (int) Math.ceil(nombreImageBriqueEnX);
+        int nbY = (int) Math.ceil(nombreImageBriqueEnY);
+
+        for (int j = 0; j < nbX; j++) {
+            for (int i = 0; i < nbY; i++) {
+                context.drawImage(imageBrique, j * tailleImageX, i * tailleImageY);
+            }
+        }
     }
 
-    @Override
-    public void draw(GraphicsContext context) {}
 }
