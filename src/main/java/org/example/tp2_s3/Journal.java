4c123b8d6df79f3b7ccde8fe175097b9329a6862
moute40 <famajor77@gmail.com>
2025-11-27 13:57:13 -0500

    journal methode draw

--- src/main/java/org/example/tp2_s3/Journal.java
+++ src/main/java/org/example/tp2_s3/Journal.java
@@ -2,16 +2,18 @@ package org.example.tp2_s3;
 
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public class Journal extends ObjetEnMouvement {
 
+    protected Image imgJournal = new Image("/journal.png");
     public Journal(Point2D position, Point2D taille, Point2D vitesse, Point2D acceleration) {
         super(position, taille, vitesse, acceleration);
 
     }
 
     @Override
-    public void draw(GraphicsContext context) {
+    public void draw(GraphicsContext context,Camera camera) {
 
     }
 }
