7086c582384e861860454e939a918a1aed3d5dd9
moute40 <famajor77@gmail.com>
2025-11-28 12:31:40 -0500

    ArrierePlan

--- src/main/java/org/example/tp2_s3/ArrierePlan.java
+++ src/main/java/org/example/tp2_s3/ArrierePlan.java
@@ -6,36 +6,25 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 
-public class ArrierePlan {
+public class ArrierePlan implements Drawable {
     private Point2D tailleBrique = new Point2D(192, 96);
-    private Image imageBrique = new Image(getClass().getResourceAsStream("/brique.png"));
+    private Image imageBrique = new Image(("/brique.png"));
 
     private double positionMurEnX = 0;
-    private Point2D velociteMur = new Point2D(0,0); // vitesse visuelle de base du décor
-
-    public void update(double vitesseCamera, double deltaTemps) {
-        // vitesse relative décor / caméra
-        double vitesseRelative = velociteMur.getX() - vitesseCamera;
-        positionMurEnX += -vitesseRelative * deltaTemps;
-
-        positionMurEnX %= tailleBrique.getX();
-        if (positionMurEnX < 0){
-            positionMurEnX += tailleBrique.getX();
-        }
-    }
 
 
-    public void draw(GraphicsContext context) {
+    @Override
+    public void draw(GraphicsContext context, Camera camera) {
 
         int nbX = (int)Math.ceil(MainJavaFx.WIDTH / tailleBrique.getX()) + 2;
         int nbY = (int)Math.ceil(MainJavaFx.HEIGHT / tailleBrique.getY());
 
         for (int i = 0; i < nbY; i++) {
             for (int j = 0; j < nbX; j++) {
-                double x = j * tailleBrique.getX() - positionMurEnX;
-                double y = i * tailleBrique.getY();
-                context.drawImage(imageBrique, x, y);
+                context.drawImage(imageBrique, camera.coordoEcran(j*tailleBrique.getX()),i*tailleBrique.getY() );
             }
         }
     }
+
+
 }
