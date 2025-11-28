514190cf774a45b572b5ccb2af4bfa0184c8f122
moute40 <famajor77@gmail.com>
2025-11-27 19:57:07 -0500

    journal methode draw

---
+++ src/main/java/org/example/tp2_s3/Maison.java
@@ -0,0 +1,37 @@
+package org.example.tp2_s3;
+
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Maison {
+    Porte porte;
+
+    ArrayList<Fenetre> fenetre;
+
+    BoiteAuxLettres boiteAuLettre;
+
+    Random aleatoire = new Random();
+
+    public Maison(Porte porte) {
+        this.porte = porte;
+        int nbFenetre = aleatoire.nextInt(2);
+        for (int i = 0; i < nbFenetre; i++) {
+            fenetre.add(new Fenetre());
+        }
+        this.boiteAuLettre = new BoiteAuxLettres();
+    }
+
+    public void drawMaison(GraphicsContext context, Camera camera) {
+        porte.draw(context, camera);
+        boiteAuLettre.draw(context, camera);
+        for (Fenetre fenetre : fenetre) {
+            fenetre.draw(context, camera);
+        }
+    }
+
+    public void updateCollisionAvec(){
+
+    }
+}
