3e8029152cdba6c451286a8b2fd371e0e3096b42
moute40 <famajor77@gmail.com>
2025-11-28 12:44:16 -0500

    ArrierePlan

--- src/main/java/org/example/tp2_s3/ArrierePlan.java
+++ src/main/java/org/example/tp2_s3/ArrierePlan.java
@@ -4,27 +4,38 @@ package org.example.tp2_s3;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 
 
 public class ArrierePlan implements Drawable {
+
+    private static final double WORLD_WIDTH = 14*1300;
     private Point2D tailleBrique = new Point2D(192, 96);
     private Image imageBrique = new Image(("/brique.png"));
 
-    private double positionMurEnX = 0;
+
+
 
 
     @Override
     public void draw(GraphicsContext context, Camera camera) {
+        context.setFill(Color.BLACK);
 
-        int nbX = (int)Math.ceil(MainJavaFx.WIDTH / tailleBrique.getX()) + 2;
+        int nbX = (int)Math.ceil( WORLD_WIDTH / tailleBrique.getX()) + 2;
         int nbY = (int)Math.ceil(MainJavaFx.HEIGHT / tailleBrique.getY());
 
         for (int i = 0; i < nbY; i++) {
             for (int j = 0; j < nbX; j++) {
-                context.drawImage(imageBrique, camera.coordoEcran(j*tailleBrique.getX()),i*tailleBrique.getY() );
+
+                double xEcran = camera.coordoEcran(j * tailleBrique.getX());
+                double yEcran = i * tailleBrique.getY();
+
+                context.drawImage(imageBrique, xEcran, yEcran);
             }
         }
+
     }
+
 
 
 }
