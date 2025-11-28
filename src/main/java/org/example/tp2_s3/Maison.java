2b97147ddd608a2db5d1667e5d9cb245ce82cf2f
moute40 <famajor77@gmail.com>
2025-11-27 22:43:57 -0500

    finit classe Maison et fait draw Boite au lettre et Fenetre

--- src/main/java/org/example/tp2_s3/Maison.java
+++ src/main/java/org/example/tp2_s3/Maison.java
@@ -1,38 +1,51 @@
 package org.example.tp2_s3;
 
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Maison {
-    Porte porte;
 
-    ArrayList<Fenetre> fenetre;
-
-    BoiteAuxLettres boiteAuLettre;
-
-    Random aleatoire = new Random();
+    private Porte porte;
+    private ArrayList<Fenetre> fenetre;
+    private BoiteAuxLettres boiteAuLettre;
+    private Random aleatoire = new Random();
 
     public Maison(Porte porte) {
         this.porte = porte;
-        int nbFenetre = aleatoire.nextInt(2);
-        for (int i = 0; i < nbFenetre; i++) {
-            fenetre.add(new Fenetre());
+        this.fenetre = new ArrayList<>();
+
+
+        int nbFenetre = aleatoire.nextInt(3);
+
+        if (nbFenetre >= 1) {
+            fenetre.add(new Fenetre(new Point2D(porte.getDroite() + 300, 50)));
         }
-        this.boiteAuLettre = new BoiteAuxLettres();
+
+        if (nbFenetre == 2) {
+            fenetre.add(new Fenetre(new Point2D(porte.getDroite() + 600, 50)));
+        }
+
+
+        int valeur = aleatoire.nextInt(51) + 20; // 20 à 70
+        this.boiteAuLettre = new BoiteAuxLettres(
+                new Point2D(porte.getDroite() + 200, MainJavaFx.HEIGHT * (valeur / 100.0)));
     }
 
     public void drawMaison(GraphicsContext context, Camera camera) {
         porte.draw(context, camera);
         boiteAuLettre.draw(context, camera);
-        for (Fenetre fenetre : fenetre) {
-            fenetre.draw(context, camera);
+
+        for (Fenetre f : fenetre) {
+            f.draw(context, camera);
         }
     }
-    public void updateCollisionAvecObjetDeMaison(){
-        for (Fenetre fenetre : fenetre){
-            fenetre.interact();
+
+    public void updateCollisionAvecObjetDeMaison() {
+        for (Fenetre f : fenetre) {
+            f.interact();
         }
         boiteAuLettre.interact();
     }
